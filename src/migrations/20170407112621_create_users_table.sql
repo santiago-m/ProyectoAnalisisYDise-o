e5214106c315c0ca5554d5ea246d0972e090853c
Santiago <santimaria93@gmail.com>
2017-05-07 17:47:24 -0300

    edicion en linea de creacion de tabla. Me tiraba error

--- src/migrations/20170407112621_create_users_table.sql
+++ src/migrations/20170407112621_create_users_table.sql
@@ -1,7 +1,22 @@
+DROP TABLE IF EXISTS `users`;
+
 CREATE TABLE users (
-  id  int(11) DEFAULT NULL auto_increment PRIMARY KEY,
+  id  int(11) auto_increment PRIMARY KEY,
   username  VARCHAR(128),
   password  VARCHAR(128),
+  puntaje int(11),
   created_at DATETIME,
   updated_at DATETIME
 )ENGINE=InnoDB;
+
+
+DROP TABLE IF EXISTS `admins`;
+CREATE TABLE admins (
+  id  int(11) auto_increment PRIMARY KEY,
+  username  VARCHAR(128),
+  password  VARCHAR(128)
+)ENGINE=InnoDB;
+
+INSERT INTO admins (id, username, password) VALUES
+ (1, 'sajoma', '13794682'),
+ (2, 'matesuiso', 'contraseñaMati');
