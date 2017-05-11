90d62dc0f005c256142630fb0f08c6f0b1661168
Santiago <santimaria93@gmail.com>
2017-05-11 20:58:59 -0300

    Class Question created

---
+++ src/main/java/trivia/Question.java
@@ -0,0 +1,46 @@
+package trivia;
+
+import org.javalite.activejdbc.Model;
+
+public class Question extends Model {
+	private int cantOpciones = 1;
+
+	static{
+    	validatePresenceOf("pregunta").message("Please, provide your username");
+  	}
+
+  	public boolean checked(){
+  		if (!(this.getBoolean("leido"))) {
+  			return false;
+  		}
+  		else {
+  			return true;
+  		}
+  	}
+
+  	public void check() {
+  		this.set("leido", 1);
+  		this.saveIt();
+  	}
+
+  	public void unCheck(){
+  		this.set("leido", 0);
+  		this.saveIt();	
+  	}
+
+  	public void calcularOpciones() {
+  		if (this.get("wrong1") != null) {
+			cantOpciones++;
+		}
+		if (this.get("wrong2") != null) {
+			cantOpciones++;
+		}
+		if (this.get("wrong3") != null) {
+			cantOpciones++;
+		}
+  	}
+
+  	public int getCantOpciones() {
+  		return cantOpciones;
+  	}
+}
