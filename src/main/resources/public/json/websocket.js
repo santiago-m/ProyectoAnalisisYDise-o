b0f23e1e4da749b3639eac9dc2807ce55788d9f8
Santiago Maria <santimaria93@gmail.com>
2017-10-10 17:53:43 -0300

    js added

---
+++ src/main/resources/public/json/websocket.js
@@ -0,0 +1,14 @@
+//Establish the WebSocket connection and set up event handlers
+var webSocket = new WebSocket("ws://" + location.hostname + ":" + location.port + "/game");
+webSocket.onmessage = function (msg) { print(msg) };
+webSocket.onclose = function () { alert("WebSocket connection closed") };
+
+function print(msg) {
+	var jsonData = JSON.parse(msg.data);
+	console.log(msg);
+
+}
+
+function sendMessage() {
+	webSocket.send('hola!');
+}
