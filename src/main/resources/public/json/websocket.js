5cfa8aaeb384f2aa10cf300798bef30a370b5ff0
Santiago Maria <santimaria93@gmail.com>
2017-10-17 00:06:28 -0300

    added endPoint as session attribute.- Sending username in response

--- src/main/resources/public/json/websocket.js
+++ src/main/resources/public/json/websocket.js
@@ -1,7 +1,10 @@
+
+
 //Establish the WebSocket connection and set up event handlers
 var webSocket = new WebSocket("ws://" + location.hostname + ":" + location.port + "/game");
 webSocket.onmessage = function (msg) { print(msg) };
 webSocket.onclose = function () { alert("WebSocket connection closed") };
+webSocket.sendMessage("username: "+getCookie("username"))
 
 function print(msg) {
 	var jsonData = JSON.parse(msg.data);
@@ -9,6 +12,18 @@ function print(msg) {
 
 }
 
-function sendMessage() {
-	webSocket.send('hola!');
+function sendMessage(msg) {
+	webSocket.send(msg);
 }
+
+function getCookie(name) {
+	var nameEQ = name + "=";
+	//alert(document.cookie);
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1);
+		if (c.indexOf(nameEQ) != -1) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+} 
