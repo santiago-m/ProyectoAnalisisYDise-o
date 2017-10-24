bff785c45f76ef3fc919c32d7deabaddbe20aa55
Santiago Maria <santimaria93@gmail.com>
2017-10-23 23:12:22 -0300

    answer questions in progress

--- src/main/java/trivia/Game.java
+++ src/main/java/trivia/Game.java
@@ -262,13 +262,6 @@ public class Game extends Model{
   }
 
   public static boolean esCorrecta(int idPregunta, String respuestaDada) {
-
-    System.out.println("Llegue hasta aca!");
-    System.out.println("Llegue hasta aca!");
-    System.out.println("Llegue hasta aca!");
-    System.out.println("Llegue hasta aca!");
-    System.out.println("Llegue hasta aca!");
-
     App.openDB();
     List<Question> questions = Question.where("id = "+idPregunta+" and respuestaCorrecta = '"+respuestaDada+"'");
     if (questions.isEmpty()) {
