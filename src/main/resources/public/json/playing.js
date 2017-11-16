6171b2b409a3860d6413ea0ce281476005f6f720
Santiago Maria <santimaria93@gmail.com>
2017-11-16 15:23:37 -0300

    fixing some garbage code

--- src/main/resources/public/json/playing.js
+++ src/main/resources/public/json/playing.js
@@ -1,7 +1,4 @@
-var webSocket;
-
-var wsReady;
-var windowLoaded;
+var webSocket = new WebSocket("ws://" + location.hostname + ":" + location.port + "/game");
 
 var username;
 var opponent;
@@ -20,75 +17,51 @@ var answer4;
 
 
 window.onload = function() {
-	 webSocket = new WebSocket("ws://" + location.hostname + ":" + location.port + "/game");
 
-	webSocket.onopen = function() {
-		wsReady = true;
+	$.ajax({                                            
+    	url: '/play',    
+    	type: 'POST',
+    	dataType: "json",
+    	async:false,
 
-		$.ajax({                                            
-    		url: '/play',    
-    		type: 'POST',
-    		dataType: "json",
-    		async:false,
+  		success: function(data) {  
 
-  			success: function(data) {  
+    		console.log(data);
 
-    			console.log(data);
+    		username = data["player"];
+    		opponent = data["opponent"];
+    		idPregunta = data["ID"];
 
-    			username = data["player"];
-    			opponent = data["opponent"];
-    			idPregunta = data["ID"];
+    		cantPlayers = data["game_"+username];
+    		status = data["status_"+username];
 
-    			cantPlayers = data["game_"+username];
-    			status = data["status_"+username];
+	    	puntaje = data["puntaje_"+username];
+    		puntajeOponente = data["puntaje_"+opponent];
 
-	    		puntaje = data["puntaje_"+username];
-    			puntajeOponente = data["puntaje_"+opponent];
+    		pregunta = data["pregunta"];
+			answer1 = data["opcion 1"];
+			answer2 = data["opcion 2"];
+			answer3 = data["opcion 3"];
+			answer4 = data["opcion 4"];
 
-    			pregunta = data["pregunta"];
-				answer1 = data["opcion 1"];
-				answer2 = data["opcion 2"];
-				answer3 = data["opcion 3"];
-				answer4 = data["opcion 4"];
+			answers = [answer1, answer2, answer3, answer4];
 
-				answers = [answer1, answer2, answer3, answer4];
-
-				cantOpciones = 0;
-				if (answer1 != "") {
-					cantOpciones++;
-				}
-				if (answer2 != "") {
-					cantOpciones++;
-				}
-				if (answer3 != "") {
-					cantOpciones++;
-				}
-				if (answer4 != "") {
-					cantOpciones++;
-				}
+			cantOpciones = 0;
+			if (answer1 != "") {
+				cantOpciones++;
 			}
-		});
-
-		webSocket.send(JSON.stringify({
-  			username: username
-  		}));
-
-		if (status == "waiting") {
-			waitForTurn();
-		}
-		else {
-			updatePoints();
-			$('#questionPlace').html('');
-			$('#questionPlace').html(pregunta);
-
-			$('#answersPlace').html('');
-			for (var i = 0; i < answers.length; i++) {
-				if (answers[i] != '') {
-					$('#answersPlace').append('<p> <input id="answer'+i+'" type="radio" name="answer" value="'+answers[i]+'"> '+answers[i]+'  </p>');
-				}
+			if (answer2 != "") {
+				cantOpciones++;
+			}
+			if (answer3 != "") {
+				cantOpciones++;
+			}
+			if (answer4 != "") {
+				cantOpciones++;
 			}
 		}
-	}
+	});
+}
 
 	webSocket.onmessage = function (msg) {
 		var message = JSON.parse(msg['data']);
@@ -123,19 +96,6 @@ window.onload = function() {
 	webSocket.onclose = function () { wsReady = false; };
 }
 
-var iFrequency;
-var myInterval;
-
-// STARTS and Resets the loop if any
-function startLoop() {
-    if(myInterval > 0) clearInterval(myInterval);  // stop
-    myInterval = setInterval( "sendPing()", iFrequency );  // run
-}
-
-function sendPing() {
-    webSocket.send('PING');
-}
-
 function updatePoints() {
 	$('#pointsPlace').html('');
 	if (cantPlayers == 2) {
@@ -147,7 +107,7 @@ function updatePoints() {
 }
 
 function waitForTurn() {
-
+	$('#pointsPlace').html('');
 	$('#questionPlace').html('');
 	$('#questionPlace').html('Espere por su turno.');
 
@@ -214,6 +174,8 @@ function nextQuestion() {
 			}
 		}	
 	}
+	$('#pointsPlace').html('');
+	$('#pointsPlace').html(username + ': '+ puntaje + '<br>' + opponent + ': ' + puntajeOponente);
 }
 
 function refreshQuestion(msg) {
@@ -286,16 +248,3 @@ function sendAnswer() {
 function sendMessage(msg) {
 	webSocket.send("hello");
 }
-
-function getCookie(name) {
-	var nameEQ = name + "=";
-	//alert(document.cookie);
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1);
-		if (c.indexOf(nameEQ) != -1) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
-} 
-
